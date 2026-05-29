2601d2f178fd5ee114b5c20bf6f264c526fe6d5c
Hartej Lehal <tejlehal13@gmail.com>
2026-05-28 23:27:54 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // for(let i = 0; i<nums.length; i++){
+        //     for(let j = i + 1; j < nums.length; j++) {
+        //         let sum = nums[i] + nums[j];
+        //         if(sum == target){
+        //             return([i,j])
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // Two Pass HashMap
+        // const valObj = {}
+        // for(let i = 0; i < nums.length; i++) {
+        //     valObj[nums[i]] = i
+           
+        // }
+
+        // for(let i = 0; i<nums.length; i++) {
+        //     let diff = target - nums[i]
+        //     if(valObj[diff] !== i && valObj[diff] !== undefined) {
+        //         return [i, valObj[diff]]
+        //     }
+        // }
+        // return [];
+
+        const map = new Map();
+
+        for(let i = 0; i < nums.length; i++){
+            let diff = target - nums[i]
+            if(map.has(diff)) {
+                return [map.get(diff), i]
+            }
+            map.set(nums[i],i)
+        }
+        return []
+    }
+}
